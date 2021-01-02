4bf52327f10e9177abcd47003f946d7f43dfb904
Daniela Andrade <pipklein@hotmail.com>
2021-01-02 04:03:56 -0500

    First implementation

---
+++ Argentum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Argentum
+{
+    public partial class Argentum : Form
+    {
+        public Argentum()
+        {
+            InitializeComponent();
+        }
+
+        private void picImage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
